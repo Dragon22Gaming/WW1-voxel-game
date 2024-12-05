bacb43086155fa31196308ba572b2160057fc260
DragonDev22 <dragon22gaming.yt@gmail.com>
2024-12-05 15:15:03 +1030

    Added stuff

---
+++ new-game-project/Assets/Scripts/Menu Manager.gd
@@ -0,0 +1,14 @@
+extends Control
+@export var global = "res://Assets/Scripts/Global.gd"
+#When buttons are clicked, do stuff
+
+func _on_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
