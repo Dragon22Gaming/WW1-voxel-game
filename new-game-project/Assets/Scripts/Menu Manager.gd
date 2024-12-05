9895f18602599605bbd6186e966cc144d5b623e5
DragonDev22 <dragon22gaming.yt@gmail.com>
2024-12-05 16:07:51 +1030

    stuffin around

--- new-game-project/Assets/Scripts/Menu Manager.gd
+++ new-game-project/Assets/Scripts/Menu Manager.gd
@@ -1,10 +1,11 @@
 extends Control
 @export var global = "res://Assets/Scripts/Global.gd"
 #When buttons are clicked, do stuff
+func _ready():
+	preload("res://Assets/Scenes/arena.tscn")
 
 func _on_play_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://Assets/Scenes/arena.tscn")
 
 func _on_settings_pressed():
 	pass # Replace with function body.
